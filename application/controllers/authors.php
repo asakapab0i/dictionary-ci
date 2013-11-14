0aa358081efd2dcd21f1fe0be12a39e1f339c6f8
Bryan B <bojorquebryan@gmail.com>
2013-11-15 02:13:57 +0800

    initial commit to author module

--- application/controllers/authors.php
+++ application/controllers/authors.php
@@ -4,13 +4,34 @@ class Authors extends CI_Controller{
 
 
 	public function index(){
+		$global = self::load_global_class();
 
+		$header['page_title'] = "Authors";
+		$header['main_menu'] = $global::main_nav('authors');
+		$header['sub_menu'] = $global::sub_nav();
+		$header['word'] = '';
+
+
+
+		$this->parser->parse('template/header', $header);
+		$this->load->view('author/author_view');
+		$this->load->view('template/footer.php');
 
 	}
 
-	public function author($author){
+	public function view($author){
 
 		
+	}
+
+
+
+	//HEALDER CLASS
+	private function load_global_class(){
+		//include and initialized global class
+		include 'global_class.php';
+		$global = new Global_class();
+		return $global;
 	}
 
 
---
+++ application/views/author/author_view.php
@@ -0,0 +1,12 @@
+    <div class="container">
+    	<div class="row">
+    		<div class="col-md-8 custom-main-workplace">
+                <p>Author</p>
+    		</div>
+    		<div class="col-md-4 custom-mailing-list">
+            sidebar
+    		</div>
+    	</div>
+    <!-- {side_bar} -->
+    </div> <!-- /container -->
+    
